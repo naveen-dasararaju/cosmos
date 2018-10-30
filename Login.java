e06f3690f41ab90044414b102f506b70a63d0e7c
hemanth-raju <d.n.k.raju333@gmail.com>
2018-10-30 21:05:56 +0530

    Revert "commiting 3 files"
    This reverts commit d057e247bd051d3572f92fa447acf8228b05d957.

--- Login.java
+++ Login.java
@@ -1,9 +1,9 @@
 class login
 {
- public static void main(String args)
-  {
-    System.out.println("First line");
-    System.out.println("Adding second line");
-  }
+public static void main(String args)
+{
+System.out.println("First line");
+System.out.println("Adding second line");
+}
 }
 
